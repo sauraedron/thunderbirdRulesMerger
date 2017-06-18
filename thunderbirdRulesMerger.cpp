27e922f5f34ba17b081a4b8691e852e9087d6659
Saurabh Wankhade <sauraedron@gmail.com>
2017-06-18 22:25:05 +0530

    Pushing the dat file, and some changes in the main cpp file

--- thunderbirdRulesMerger.cpp
+++ thunderbirdRulesMerger.cpp
@@ -6,25 +6,24 @@ int main(int argv, char **argc)
 {
   if(argv==0 || argv==1)
     {
-      cout<<"usage [filtername]"
+      cout<<"usage [filtername] [location for update] [filter on] [operator] [input_file]\n";
+      return -1;
     }
-  string trm_filter_name, trm_location_for_update, trm_filter_on, trm_opeartor, trm_input_file, trm_message_destination;
-  trm_fiter_name = argv[1];
-  trm_location_for_update = argv[2];
-  trm_filter_on = argv[3];
-  trm_operator = argv[4];
-  trm_input_file = argv[5];
-  
-  //dat_file = argc[1];
-  //  filter = argc[2];
-  //  op = argc[3];
-  //  input_file = argc[4];
-  //  destination = argc[5];
-  trm_location_for_update = "/home/saurabh/.thunderbird/9jtgbmex.default/ImapMail/imap.mail.yahoo.com/messageFilterRules.dat";
-  filter = "subject";
-  op = "contains";
-  input_file = "/home/saurabh/thunderbird.dat";
-  destination = "imap://saw_wan20%40yahoo.com@imap.mail.yahoo.com/Bulk Mail";
+  string trm_filter_name, trm_location_for_update, trm_filter_on, trm_operator, trm_input_file, trm_message_destination;
+  trm_filter_name = argc[1];
+  // trm_location_for_update = argc[2];
+  // trm_filter_on = argc[3];
+  // trm_operator = argc[4];
+  // trm_input_file = argc[5];
+
+  string dat_file = argc[1];
+
+  trm_location_for_update = "/home/saurabh/.thunderbird/9jtgbmex.default/ImapMail/imap.mail.yahoo.com/msgFilterRules.dat";
+  dat_file = trm_location_for_update;
+  string filter = "subject";
+  string op = "contains";
+  string input_file = "/home/saurabh/thunderbird.dat";
+  string destination = "imap://saw_wan20%40yahoo.com@imap.mail.yahoo.com/Bulk Mail";
   try
     {
       ifstream userRead;
@@ -34,12 +33,12 @@ int main(int argv, char **argc)
       string tmp;
       vector<string> input_values;
       while(getline(userRead, tmp))
-	{	  
+	{
 	  input_values.push_back(tmp);
 	}
       userRead.close();
       //      userWrite.seekg(ios::en);
-      userWrite<<"name=\"Auto gen_"<<rand()<<"\"\n";
+      userWrite<<"name=\""<<trm_filter_name<<"\"\n";
       userWrite<<"enabled=\"yes\"\n";
       userWrite<<"type=\"17\"\n";
       userWrite<<"action=\"Move to folder\"\n";
@@ -47,16 +46,18 @@ int main(int argv, char **argc)
       userWrite<<"condition=\"";
       for(int i=0; i<input_values.size(); ++i)
 	{
-	  userWrite<<"OR (from,"<<op<<","<<input_values.at(i)<<") ";
+    size_t xx = input_values.at(i).find('@');
+
+	  userWrite<<"OR (from,"<<op<<","<<input_values.at(i).substr(xx+1, input_values.at(i).size()-xx)<<") ";
 	}
       userWrite<<"\""<<endl;
-      
+
       userWrite.close();
-      
+
     }
   catch(const exception &e)
     {
       cout<<"Caught Exception";
     }
-  
+
 }
